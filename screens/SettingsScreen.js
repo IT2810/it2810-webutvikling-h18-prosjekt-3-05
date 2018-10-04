d938c40d43054fbc5ef8a1875dc2da9c151ca678
angelaseliv <anzhelis@stud.ntnu.no>
2018-10-04 12:27:17 +0200

    Clean-up of repo for a smud start on the project. #8

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {View,
+        Text,
+        ScrollView,
+        StyleSheet,
+} from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Settings',
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.containerText}> Hello. Here are your settings. </Text>
+        </ScrollView>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#01194f',
+  },
+  containerText: {
+    paddingTop: 30,
+    fontSize: 17,
+    color: '#2e78b7',
+    lineHeight: 24,
+    textAlign: 'center',
+  }
+});
